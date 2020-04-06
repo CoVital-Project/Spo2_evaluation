f8318c61153b03fc3a03796a40c581e9f32c2444
Gianluca Truda <gianlucatruda@gmail.com>
2020-04-06 17:19:52 +0200

    Made fetch_data script more robust and user-friendly.

--- misc/fetch_data.py
+++ misc/fetch_data.py
@@ -10,44 +10,74 @@ import shutil
 
 
 DATADIR = Path('data')
-NEMCOVADIR = DATADIR / 'nemcova_data'
 SAMPLEDIR = DATADIR / 'sample_data'
-
-NEMCOVAURL = "https://www.dropbox.com/s/v77ldm3goakx2rj/nemcova_data.zip?dl=1"
+NEMCOVADIR = DATADIR / 'nemcova_data'
 SAMPLEURL = "https://www.dropbox.com/s/dl8o9htbfsmzl50/sample_data.zip?dl=1"
+NEMCOVAURL = "https://www.dropbox.com/s/v77ldm3goakx2rj/nemcova_data.zip?dl=1"
+
+dir_url_dict = {
+    SAMPLEDIR: SAMPLEURL,
+    NEMCOVADIR: NEMCOVAURL,
+}
 
 
 def check_datadir():
-    # Check that datadir exists and has videos
-    if os.path.exists(NEMCOVADIR) and os.path.exists(SAMPLEDIR):
-        return True
+    """Checks if data directory is complete. If not, return list
+        of directories to update. Makes missing directories automatically.
 
-    os.makedirs(NEMCOVADIR)
-    os.makedirs(SAMPLEDIR)
-    return False
+    Returns
+    -------
+    List
+        A list of the directories that are incomplete.
+    """
+
+    dirs_to_fetch = []
+    for directory in list(dir_url_dict.keys()):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        if len(os.listdir(directory)) == 0:
+            dirs_to_fetch.append(directory)
+    return dirs_to_fetch
 
 
-def fetch_data():
-    # Fetch video data from Dropbox links
-    # See https://stackoverflow.com/questions/9419162/download-returned-zip-file-from-url#14260592
-    for url, path in {SAMPLEURL: SAMPLEDIR, NEMCOVAURL: NEMCOVADIR}.items():
+def fetch_data(dirs_to_fetch: list):
+    """Fetch video data from Dropbox links, unzip, and clean up.
+
+    Retrieval and unzipping code comes from https://stackoverflow.com/questions/9419162/download-returned-zip-file-from-url#14260592
+
+    Parameters
+    ----------
+    dirs_to_fetch : list
+        A list of directories that need updating.
+    """
+
+    fetch_dict = {k: v for k, v in dir_url_dict.items() if k in dirs_to_fetch}
+
+    for path, url in fetch_dict.items():
         print(f"Retrieving from {url}")
         try:
             r = requests.get(url)
             z = zipfile.ZipFile(io.BytesIO(r.content))
             z.extractall(path)
 
-            # Do some cleanup of junk dirs
-            shutil.rmtree(path/'__MACOSX')
-
         except Exception as e:
             print(f"Error retrieving {url}:\n{e}")
-    print("Done")
+
+        # Do some cleanup of junk dirs
+        junk_path = path/'__MACOSX'
+        try:
+            shutil.rmtree(junk_path)
+        except Exception as e:
+            print(f"Error cleaning up '{junk_path}'")
 
 
 if __name__ == '__main__':
 
-    if check_datadir():
-        print(f"Data in {DATADIR} appears intact!")
+    to_fetch = check_datadir()
+    if len(to_fetch) > 0:
+        print(f"Fetching data for: {to_fetch}")
+        fetch_data(to_fetch)
     else:
-        fetch_data()
+        print("It doesn't seem like you need to fetch anything.")
+
+    print("Done.")
