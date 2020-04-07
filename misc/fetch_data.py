56578f29cd29ec37359f3fcd96435b857d2ace44
Gianluca Truda <gianlucatruda@gmail.com>
2020-04-07 14:50:33 +0200

    Formatted using black.

--- misc/fetch_data.py
+++ misc/fetch_data.py
@@ -9,16 +9,13 @@ import io
 import shutil
 
 
-DATADIR = Path('data')
-SAMPLEDIR = DATADIR / 'sample_data'
-NEMCOVADIR = DATADIR / 'nemcova_data'
+DATADIR = Path("data")
+SAMPLEDIR = DATADIR / "sample_data"
+NEMCOVADIR = DATADIR / "nemcova_data"
 SAMPLEURL = "https://www.dropbox.com/s/dl8o9htbfsmzl50/sample_data.zip?dl=1"
 NEMCOVAURL = "https://www.dropbox.com/s/v77ldm3goakx2rj/nemcova_data.zip?dl=1"
 
-dir_url_dict = {
-    SAMPLEDIR: SAMPLEURL,
-    NEMCOVADIR: NEMCOVAURL,
-}
+dir_url_dict = {SAMPLEDIR: SAMPLEURL, NEMCOVADIR: NEMCOVAURL}
 
 
 def check_datadir():
@@ -64,14 +61,14 @@ def fetch_data(dirs_to_fetch: list):
             print(f"Error retrieving {url}:\n{e}")
 
         # Do some cleanup of junk dirs
-        junk_path = path/'__MACOSX'
+        junk_path = path / "__MACOSX"
         try:
             shutil.rmtree(junk_path)
         except Exception as e:
             print(f"Error cleaning up '{junk_path}'")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     to_fetch = check_datadir()
     if len(to_fetch) > 0:
