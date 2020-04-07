8d0e52fa04a4faa0a37edb2b619b6ec0be38b6c2
Malcolm Mielle <malcolm.mielle@protonmail.com>
2020-04-07 16:29:48 +0200

    malcolm: same data format as #19
    Panda Dataframe is the same format everywhere now

--- preprocessing/data_loader_pandas.py
+++ preprocessing/data_loader_pandas.py
@@ -41,9 +41,23 @@ class Spo2DatasetPandas(data_loader.Spo2Dataset):
         self.number_of_videos = len(self.video_folders)
         #print(self.number_of_videos)
         
-        data_list = []
-        column = []
-        nb_video = 1
+        data_list_red_mean = []
+        data_list_blue_mean = []
+        data_list_green_mean = []
+        data_list_red_std = []
+        data_list_blue_std = []
+        data_list_green_std = []
+        data_frame_number = []
+        data_video_id = []
+        data_sample_source = []
+        
+        data_sample_label_id = []
+        data_label_source = []
+        data_hr =[]
+        data_spo2 = []
+        
+        #column = []
+        nb_video = 0
         for video in self.video_folders:
             
             ppg_red_mean = list()
@@ -52,11 +66,13 @@ class Spo2DatasetPandas(data_loader.Spo2Dataset):
             ppg_red_std = list()
             ppg_green_std = list()
             ppg_blue_std = list()
+            frame_numbers = list()
+            video_id = list()
+            sample_source = list()
             print("Loading video:", nb_video)
             
             #print(data_list)
             
-            nb_video += 1
             ppg = []
             video_path = os.path.join(self.data_path, video)
             video_file = os.path.join(video_path, [file_name for file_name in os.listdir(video_path) if file_name.endswith('mp4')][0])
@@ -64,7 +80,7 @@ class Spo2DatasetPandas(data_loader.Spo2Dataset):
             meta = {}
             meta['video_fps'] = vidcap.get(cv2.CAP_PROP_FPS)
             (grabbed, frame) = vidcap.read()
-            frame_count = 1
+            frame_count = 0
             while grabbed:
                 blue_channel_mean, blue_channel_std, green_channel_mean, green_channel_std, red_channel_mean, red_channel_std = self.transform_faster(frame)
                 
@@ -74,10 +90,13 @@ class Spo2DatasetPandas(data_loader.Spo2Dataset):
                 ppg_blue_std.append(blue_channel_std)
                 ppg_green_std.append(green_channel_std)
                 ppg_red_std.append(red_channel_std)
+                frame_numbers.append(frame_count)
+                video_id.append(nb_video)
+                sample_source.append(video_file)
                 
                 ppg.append(frame)
                 (grabbed, frame) = vidcap.read()
-                if(frame_count % 50 == 0):
+                if(frame_count % 50 == 0 and frame_count != 0):
                     print("Frame:", frame_count)
                     break
                 frame_count += 1
@@ -86,29 +105,47 @@ class Spo2DatasetPandas(data_loader.Spo2Dataset):
                 
             #print("b", data_list)
                 
-            data_list.append(ppg_blue_mean)
+            data_list_blue_mean = data_list_blue_mean + ppg_blue_mean
             #print(data_list)
-            data_list.append(ppg_blue_std)
-            data_list.append(ppg_green_mean)
-            data_list.append(ppg_green_std)
-            data_list.append(ppg_red_mean)
-            data_list.append(ppg_red_std)
-            data_list.append([int(ground_truth['SpO2'])])
-            data_list.append([int(ground_truth['HR'])])
-            data_list.append([meta['video_fps']])
+            data_list_blue_std = data_list_blue_std + ppg_blue_std
+            data_list_green_mean = data_list_green_mean + ppg_green_mean
+            data_list_green_std = data_list_green_std + ppg_green_std
+            data_list_red_mean = data_list_red_mean + ppg_red_mean
+            data_list_red_std = data_list_red_std + ppg_red_std
+            data_frame_number = data_frame_number + frame_numbers
+            data_video_id = data_video_id + video_id
+            data_sample_source = data_sample_source + sample_source
             
-            column = column + ["blue", "blue std", "green", "green std", "red", "red std", "spo2_gt", "hr_gt", "fps"]
+            data_sample_label_id = data_sample_label_id + [nb_video]
+            data_label_source = data_label_source + [video_path]
+            data_hr = data_hr + [int(ground_truth['HR'])]
+            data_spo2 = data_spo2 + [int(ground_truth['SpO2'])]
+            #data_list.append([int(ground_truth['SpO2'])])
+            #data_list.append([int(ground_truth['HR'])])
+            #data_list.append([meta['video_fps']])
+            nb_video += 1
             
-            #print(data_list)
-                        
+        ## making the data
+        column = ["mean_red", "std_red", "mean_green", "std_green", "mean_blue", "std_blue", "frame", "sample_id", "sample_source"]
+        data_list = [data_list_red_mean, data_list_red_std, data_list_green_mean, data_list_green_std, data_list_blue_mean, data_list_blue_std,  data_frame_number, data_video_id, data_sample_source]
         print(column)
-        self.data =  pd.DataFrame(data_list, column)
-        #self.data = self.data.T
-        print(self.data)
+        self.sample_data =  pd.DataFrame(data_list, column).T
         
-    def get_video(self, index):
-        index_frame = 9 * index
-        return self.data.iloc[index_frame:index_frame+9]
+        ## Making the labels
+        
+        column_labels = ["sample_id", "path", "HR", "SpO2"]
+        data_list_labels = [data_sample_label_id, data_label_source, data_hr, data_spo2]
+        self.ground_truths_sample =  pd.DataFrame(data_list_labels, column_labels).T
+        
+        
+        #self.data = self.data.T
+        print(self.sample_data)
+        #self.data = self.data.T
+        print(self.ground_truths_sample)
+        
+    #def get_video(self, index):
+        #index_frame = 9 * index
+        #return self.data.iloc[index_frame:index_frame+9]
 
 
 if __name__== "__main__":
