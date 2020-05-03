02677d3718f3ddc8b8108fe0206a45391cea70e9
Yoni Schirris <yschirris@gmail.com>
2020-05-03 16:36:30 +0200

    add reading from csv, add plotting for bandman plot

--- src/evaluation/Spo2_evaluation.py
+++ src/evaluation/Spo2_evaluation.py
@@ -7,6 +7,8 @@ import sys
 sys.path.append('../..')
 from spo2evaluation.evaluation import blant_altman
 from argparse import ArgumentParser
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -21,18 +23,21 @@ def main():
     args = parser.parse_args()
     print("Launching the evaluatio for", args.data_file)
 
+    results_df = pd.read_csv(args.data_file)
+
+
     # Extract Camera data
-    Data_camera = []
+    Data_camera = results_df['spo2_pred']
 
     # Extract GT data
-    Data_gt = []
+    Data_gt = results_df['spo2_gt']
 
     # Do statistical estimation
     least_square_slope_1, least_square_y_intercept_1, R_and_P_1 = blant_altman.Correlation(
         Data_camera, Data_gt
     )
     mean_difference_1, std_difference_1, upper_limit_1, lower_limit_1 = blant_altman.BlandAltmanPlot(
-        Data_camera, Data_gt
+        Data_camera, Data_gt, fig_num=1
     )
 
 
